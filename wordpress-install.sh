06982cff13c83103895c55219e61667346b6a04e
roopalya <roopalya@gmail.com>
2018-01-04 23:04:58 +0000

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -15,6 +15,7 @@ define('WP_DEBUG_DISPLAY', true);
 define('WP_MEMORY_LIMIT', '256M');
 PHP
 wp --allow-root core install --path=$WP_NAME --url=$BASE_URL --title=$WP_NAME --admin_user=test --admin_email=test@localhost.com
+wp --allow-root post delete 1
 wp --allow-root post create ~/7digital/mypost --post_title='Samplepost' --post_status=publish
 wp --allow-root media import ~/7digital/sunflower.jpg --title="A downloaded picture" --featured_image
 ATTACHMENT_ID="$(wp --allow-root media import ~/7digital/sunflower.jpg --porcelain)"
